83c07d5bf004e20e8bb598bdf4f75bfa776c0fa9
Joseph Kristiano <o.joseph.k@gmail.com>
2021-10-08 22:47:49 +0700

    Navigation basic

--- app/src/main/java/com/joseph18/ifubaya/adv160418015utsrecipedia/view/MainActivity.kt
+++ app/src/main/java/com/joseph18/ifubaya/adv160418015utsrecipedia/view/MainActivity.kt
@@ -2,11 +2,26 @@ package com.joseph18.ifubaya.adv160418015utsrecipedia.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.joseph18.ifubaya.adv160418015utsrecipedia.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController :NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+
+        bottomNav.setupWithNavController(navController)
     }
 }
